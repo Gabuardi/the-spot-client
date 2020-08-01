ae1699437747c5805e13543624ecd66907881c36
Josué Gabuardi <jgabuardi@beenverified.com>
2020-08-01 16:59:27 -0600

    Add funcionality

--- landing/script.js
+++ landing/script.js
@@ -1,3 +1,23 @@
+const $logInOption = $('#login-option');
+const $signInOption = $('#signin-option');
+const $logInForm = $('#login-form');
+const $signInForm = $('#signin-form');
+
+$logInOption.click(() => {
+  $signInOption.removeClass('active');
+  $logInOption.addClass('active');
+  $signInForm.addClass('d-none');
+  $logInForm.removeClass('d-none');
+});
+
+
+$signInOption.click(() => {
+  $logInOption.removeClass('active');
+  $signInOption.addClass('active');
+  $logInForm.addClass('d-none');
+  $signInForm.removeClass('d-none');
+});
+
 $('#user-type-switch').click(() => {
   $('#user-type-switch input').click();
 });
